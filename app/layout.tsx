c2362acb361fe1f6445d49ccc61c653c9358750a
Ianszzkieyyy <ian.agustin071729@gmail.com>
2025-10-08 23:57:03 +0800

    implemented simple livekit room

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
+  title: "Prepple AI",
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
